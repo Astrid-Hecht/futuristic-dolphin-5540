3907a5223e1a615f249f2b6ea5725bdf66219e4f
Astrid Hecht <silentaviary@gmail.com>
2022-09-12 10:22:45 -0600

    test: Write US 2 model tests

--- spec/models/mechanic_spec.rb
+++ spec/models/mechanic_spec.rb
@@ -1,8 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Mechanic, type: :model do
-  xdescribe 'relationships' do
-    it { should have_many(:rides) }
+  describe 'relationships' do
+    it { should have_many(:mechanic_rides) }
+    it { should have_many(:rides).through(:mechanic_rides) }
   end
 
   describe 'class methods' do
@@ -14,6 +15,21 @@ RSpec.describe Mechanic, type: :model do
 
     it '#avg_exp' do 
       expect(Mechanic.avg_exp).to eq(((@mech_1.exp + @mech_2.exp + @mech_3.exp).to_f / 3).round(2))
+    end
+  end
+
+  describe 'instance methods' do
+    it 'can sort open rides by desc thrill rating' do
+      @park = AmusementPark.create(name: 'Seven Flags', admission_cost: 80)
+      @mech_1 = Mechanic.create(name: 'Joey', exp: 10)
+      @ride_1 = @park.rides.create(name: "Big Twister", thrill_rating: 5, open: true)
+      @ride_2 = @park.rides.create(name: "Afternoon Breeze", thrill_rating: 10, open: true)
+      @ride_3 = @park.rides.create(name: "Batman Swing", thrill_rating: 3, open: false)
+      MechanicRide.create(mechanic_id: @mech_1.id, ride_id: @ride_1.id)
+      MechanicRide.create(mechanic_id: @mech_1.id, ride_id: @ride_2.id)
+      MechanicRide.create(mechanic_id: @mech_1.id, ride_id: @ride_3.id)
+
+      expect(@mech_1.rides.thrill_sort).to eq([@ride_2, @ride_1])
     end
   end
 end
