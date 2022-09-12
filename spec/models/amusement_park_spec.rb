06b7c46619f362c8e02afd6b29628f64630e9b28
Astrid Hecht <silentaviary@gmail.com>
2022-09-12 11:26:41 -0600

    test: Write park model methods

--- spec/models/amusement_park_spec.rb
+++ spec/models/amusement_park_spec.rb
@@ -4,4 +4,22 @@ RSpec.describe AmusementPark, type: :model do
   describe 'relationships' do
     it { should have_many(:rides) }
   end
+
+  describe 'instance methods' do
+    before :each do 
+      @park = AmusementPark.create(name: 'Seven Flags', admission_cost: 80)
+      @ride_1 = @park.rides.create(name: "Big Twister", thrill_rating: 5, open: true)
+      @ride_2 = @park.rides.create(name: "Afternoon Breeze", thrill_rating: 10, open: true)
+      @ride_3 = @park.rides.create(name: "Batman Swing", thrill_rating: 3, open: false)
+    end
+
+    it '#avg_thrill' do 
+      expect(@park.avg_thrill).to eq(((@ride_1.thrill_rating + @ride_2.thrill_rating + @ride_3.thrill_rating).to_f / 3).round(2))
+    end
+
+    it '#alpha_order' do 
+      expect(@park.alpha_order).to eq([@ride_2, @ride_3, @ride_1])
+    end
+  
+  end
 end
